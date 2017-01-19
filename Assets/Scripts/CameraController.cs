5659811732e2d3768e1b4945e0db0e08fae41688
MatthewKarami <mattk@winteq.com>
2017-01-18 20:29:27 -0800

    Small things

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+
+    private Vector3 offset;
+
+    void Start () {
+        offset = this.transform.position - player.transform.position;
+    }
+	
+	void LateUpdate () {
+        this.transform.position = player.transform.position + offset;
+    }
+}
