3a7ae841273622bdd8e425ae84c6343d2b2a1e2c
MatthewKarami <mattk@winteq.com>
2017-01-18 17:41:58 -0800

    basic setup 2

---
+++ Assets/Scripts/Player_1_Controll.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_1_Controll : MonoBehaviour {
+
+    public double moveSpeed;
+    public float jumpScale;
+
+    private Rigidbody2D rb;
+    private float distToGround;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        distToGround = rb.GetComponent<BoxCollider2D>().bounds.extents.y;
+    }
+
+    void Update()
+    {
+        // Left and right movement
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0);
+        this.transform.Translate(movement);
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            rb.AddForce(new Vector3(0, jumpScale));
+        }
+    }
+
+    // Checks to see if Player_1 is touching ground
+    private bool isGrounded()
+    {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+    }
+
+}
